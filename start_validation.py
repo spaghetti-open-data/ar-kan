b7329850313efb9b90bbce5fa412a810e80a4d8d
root <sirmmo@gmail.com>
2016-05-07 12:56:51 +0200

    reflection

--- start_validation.py
+++ start_validation.py
@@ -4,59 +4,62 @@ import argparse
 import ckanapi
 import requests
 
-def main():
-    parser = argparse.ArgumentParser(description="AR-KAN CKAN-API validator.")
-    parser.add_argument('-b', '--baseurl', type=str, required=True)
-    parser.add_argument('-k', '--api-key', type=str, required=False)
+default_tasks = ["existence", "validate_csv"]
 
-    parser.add_argument("-t", '--tasks', type=str, nargs="*", default=["existence"])
+class CkanTester():
 
-    parser.add_argument("-l", "--list-tasks", type=bool)
-    
-    args = parser.parse_args()
+    def main(self):
+        parser = argparse.ArgumentParser(description="AR-KAN CKAN-API validator.")
+        parser.add_argument('-b', '--baseurl', type=str, required=True)
+        parser.add_argument('-k', '--api-key', type=str, required=False)
 
-    if args.list_tasks:
-        return ["existence", "validate_csv"]
+        parser.add_argument("-t", '--tasks', type=str, nargs="*", default=default_tasks)
 
-    baseurl = args.baseurl
-    api_key = args.api_key
+        parser.add_argument("-l", "--list-tasks", type=bool)
+        
+        args = parser.parse_args()
 
+        if args.list_tasks:
+            return default_tasks
 
-    print "connecting: " + baseurl
-    ckan = ckanapi.RemoteCKAN(baseurl,
-        apikey = api_key,
-        user_agent='ckanapivalidator/1.0 (+'+baseurl+')')
-    print "connected: " + baseurl
-
-    report = []
-
-    for package in ckan.action.package_list():
-        p_meta = ckan.action.package_show(id=package)
-        for resource in p_meta.get("resources"):
-            r_meta = ckan.action.resource_show(id=resource.get("id"))
-            for t in args.tasks:
-                print "running: " + t + " on: " + package
-                res_state = {"results":{}, "package":p_meta, "resource":r_meta}
-                existence(p_meta, r_meta, res_state)
-                validate_csv(p_meta, r_meta, res_state)
-                print "ran: " + t + " on: " + package
-                report.append(res_state)
-
-    print report
+        baseurl = args.baseurl
+        api_key = args.api_key
 
 
-def existence(package_meta, resource_meta, return_state):
-    print package_meta
-    print resource_meta
-    resp = requests.get(resource_meta.get("url"))
-    print resp
-    return_state["results"]["existence"] = "valid"
+        print "connecting: " + baseurl
+        ckan = ckanapi.RemoteCKAN(baseurl,
+            apikey = api_key,
+            user_agent='ckanapivalidator/1.0 (+'+baseurl+')')
+        print "connected: " + baseurl
 
-def validate_csv(package_meta, resource_meta, return_state):
-    return_state["results"]["validate_csv"] = "valid"
+        report = []
 
+        for package in ckan.action.package_list():
+            p_meta = ckan.action.package_show(id=package)
+            for resource in p_meta.get("resources"):
+                r_meta = ckan.action.resource_show(id=resource.get("id"))
+                for t in args.tasks:
+                    print "running: " + t + " on: " + package
+                    res_state = {"results":{}, "package":p_meta, "resource":r_meta}
+                    getattr(self, t)(p_meta, r_meta, res_state)
+                    print "ran: " + t + " on: " + package
+                    report.append(res_state)
+
+        print report
+
+    def existence(self, package_meta, resource_meta, return_state):
+        print resource_meta.get("url")
+        resp = requests.get(resource_meta.get("url"))
+        if resp.status_code in [200,201]:
+            return_state["results"]["existence"] = {"result":"valid"}
+        else: 
+            return_state["results"]["existence"] = {"details":resp.status_code, "result":"not_valid"}
+
+    def validate_csv(self, package_meta, resource_meta, return_state):
+        return_state["results"]["validate_csv"] = {"result":"valid"}
 
 
 if __name__ == '__main__':
-    main()
+    ct = CkanTester()
+    ct.main()
 
